5ee519594acc3c7018774a94f2e70d7497330b55
Joe DeCapo <joe@polka.cat>
2021-06-12 11:03:39 -0500

    wip

--- script/update_dependencies.py
+++ script/update_dependencies.py
@@ -5,62 +5,58 @@ import sys
 from pathlib import Path
 from typing import List
 
-repo_dir: Path = Path(__file__).resolve().parent.parent.resolve()
-
+modizer_repo_dir: Path = Path(__file__).resolve().parent.parent.resolve()
+external_repo_dir: Path = modizer_repo_dir.parent / 'chiptunes' / 'external'
 
 class Project(object):
     """Project class
 
-    :ivar str commit_hash: Git commit hash
-    :ivar list categories: Change categories
-    :ivar str message: Change message
-    :ivar str asana_url: Asana url if present
-    :ivar str crashlytics_url: Crashlytics url if present
-    :ivar str bugsnag_url: Bugsnag url if present
+    :ivar Path modizer_dir: Path to dependency directory in modizer
+    :ivar Path upstream_dir: Path to dependency directory in external
     """
 
-    def __init__(self, modizer_dir: str, upstream_dir: str):
+    def __init__(self, modizer_dependency_dir: str, upstream_dependency_dir: str):
         """Change __init__
 
         :param str content: Change content
         """
 
-        self.modizer_dir: Path = Path(modizer_dir)
-        self.upstream_dir: Path = Path(upstream_dir)
+        self.modizer_dir: Path = modizer_repo_dir / modizer_dir
+        self.upstream_dir: Path = external_repo_dir / upstream_dependency_dir
 
 
 def main() -> None:
     projects = [
-        Project('Adplug', ''),
-        Project('AHX', ''),
-        Project('asap', ''),
-        Project('ffmpeg-kit-audio-4.4.LTS-ios-framework', ''),
-        Project('freeverb', ''),
-        Project('highlyexperimental', ''),
-        Project('HighlyQuixotic', ''),
-        Project('highlytheoritical', ''),
-        Project('LibAtrac9-master', ''),
-        Project('libGME', ''),
-        Project('libgsf', ''),
-        Project('libkss', ''),
-        Project('libLazyusf', ''),
-        Project('libopenmpt', ''),
-        Project('libpsflib', ''),
-        Project('libtim', ''),
-        Project('libvgmplay', ''),
-        Project('libvgmstream', ''),
-        Project('libxmp', ''),
-        Project('libxsf', ''),
-        Project('libxsf_snsf', ''),
-        Project('mdxplay', ''),
-        Project('node-lame-master', ''),
-        Project('pmdmini', ''),
-        Project('sc68', ''),
-        Project('sid', ''),
-        Project('snsf', ''),
-        Project('STSound', ''),
-        Project('uade-2.13', ''),
-        Project('v2mplayer', '')
+        Project('Adplug', 'adplug'),
+        Project('AHX', 'hivelytracker'),
+        Project('asap', 'asap'),
+        Project('ffmpeg-kit-audio-4.4.LTS-ios-framework', 'ffmpeg-kit'),
+        Project('freeverb', 'Freeverb/freeverb'),
+        Project('highlyexperimental', 'webpsx'),
+        Project('HighlyQuixotic', 'Highly_Quixotic'),
+        Project('highlytheoritical', 'Highly_Theoretical'),
+        Project('LibAtrac9-master', 'LibAtrac9'),
+        Project('libGME', 'libgme'),
+        Project('libgsf', 'libgsf'),
+        Project('libkss', 'libkss'),
+        Project('libLazyusf', 'lazyusf2'),
+        Project('libopenmpt', 'openmpt'),
+        Project('libpsflib', 'psflib'),
+        Project('libtim', 'libtim'),
+        Project('libvgmplay', 'vgmplay'),
+        Project('libvgmstream', 'vgmstream'),
+        Project('libxmp', 'libxmp'),
+        Project('libxsf', 'in_xsf'),
+        Project('libxsf_snsf', 'in_xsf'),
+        Project('mdxplay', 'mdxmini'),
+        Project('node-lame-master', 'node-lame'),
+        Project('pmdmini', 'pmdmini'),
+        Project('sc68', 'sc68-2.2.1'),
+        Project('sid', 'sidplayfp'),
+        Project('snsf', 'in_xsf'),
+        Project('STSound', 'StSound'),
+        Project('uade-2.13', 'uade-2.13'),
+        Project('v2mplayer', 'v2m-player')
     ]
 
 if __name__ == '__main__':
