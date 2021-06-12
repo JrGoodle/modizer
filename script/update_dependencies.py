9333cc801622d8bac302d55aaf059d803b662f4b
Joe DeCapo <joe@polka.cat>
2021-06-12 10:26:34 -0500

    Add script to update dependencies

---
+++ script/update_dependencies.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import re
+import sys
+from pathlib import Path
+from typing import List
+
+from collections import defaultdict
+
+repo_dir: Path = Path(__file__).resolve().parent.parent.resolve()
+
+
+def main() -> None:
+    print()
+
+
+if __name__ == '__main__':
+    main()
